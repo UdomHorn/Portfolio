2f10217da552fb4343e5e7c1d2f8b0074d97e93e
udom04 <udomhorn04@gmail.com>
2025-01-15 22:50:06 +0700

    decrease paddint text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
       <div className="  text-start md:w-2/3 ">
 
       <h1 className="font-bold text-7xl max-xl:text-6xl-5xl max-md:text-4xl  text-black ">Hi, I am Udom</h1>
-      <div className=" bg-gray-100 rounded-md mt-8 px-8 py-8 ">
-        <p className="font-medium text-lg mt-4  text-gray-600 ">As a <span className="font-bold">Junior Front-End Developer</span>, I am passionate about creating visually with Notion
+      <div className=" bg-gray-100 rounded-md mt-8 p-8 ">
+        <p className="font-medium text-lg  text-gray-600 ">As a <span className="font-bold">Junior Front-End Developer</span>, I am passionate about creating visually with Notion
         appealing and responsive web applications. With a solid foundation in HTML, CSS, Javascript, I continuously strive to expand my skill and stay updated with the latest web technologies. I am eager to contribute my creativity and problem-solving abilities to building user-frienly, intuitive interfaces while growing within a collaborative team environment.</p>
       </div>
         
