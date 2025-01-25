5884c4b9e56a88ef4fa647daa0ac53731c836532
udom04 <udomhorn04@gmail.com>
2025-01-26 00:46:24 +0700

    replace cartoon-log by my photo

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,20 +2,20 @@
 
 const Hero = () => {
   return (
-    <section id="home" className=" sm:px-16  px-8  pt-28 flex max-md:flex-col gap-20 justify-center items-center  font-Inter">
+    <section id="home" className=" sm:px-16  px-8  pt-28 flex max-md:flex-col gap-10 justify-center items-center  font-Inter ">
 
-      <div className="  text-start md:w-2/3 ">
+      <div className="  text-start md:w-2/3  ">
 
-      <h1 className="font-bold text-7xl max-xl:text-6xl-5xl max-md:text-4xl  text-black ">Hi, I am Udom</h1>
-      <div className=" bg-gray-100 rounded-md mt-8 p-8 ">
+      <h1 className="font-bold text-6xl max-xl:text-5xl max-md:text-4xl  text-black ">Hi, I am Udom</h1>
+      <div className=" bg-gray-100 rounded-md mt-8 ">
         <p className="font-medium text-lg  text-gray-600 ">As a <span className="font-bold">Junior Front-End Developer</span>, I am passionate about creating visually with Notion
         appealing and responsive web applications. With a solid foundation in HTML, CSS, Javascript, I continuously strive to expand my skill and stay updated with the latest web technologies. I am eager to contribute my creativity and problem-solving abilities to building user-frienly, intuitive interfaces while growing within a collaborative team environment.</p>
       </div>
         
       </div>
 
-      <div className="">
-        <img src="cartoon.png" alt="Profile"
+      <div className="w-[350px] pt-16">
+        <img src="me.jpg" alt="Profile" className="rounded-full"
          />
       </div>
     </section>
